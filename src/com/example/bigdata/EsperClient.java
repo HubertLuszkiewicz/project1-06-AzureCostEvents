5c1f26f7bad5607f3f27acad7c8eef0c1c924836
HubertLuszkiewicz <hluszkiewicz@gmail.com>
2025-04-07 08:28:25 +0200

    Added solutions

--- src/com/example/bigdata/EsperClient.java
+++ src/com/example/bigdata/EsperClient.java
@@ -20,14 +20,15 @@ import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
 import static net.datafaker.transformations.Field.field;
+import java.util.Random;
 
 public class EsperClient {
     public static void main(String[] args) throws InterruptedException {
         int noOfRecordsPerSec;
         int howLongInSec;
         if (args.length < 2) {
-            noOfRecordsPerSec = 2;
-            howLongInSec = 5;
+            noOfRecordsPerSec = 100;
+            howLongInSec = 70;
         } else {
             noOfRecordsPerSec = Integer.parseInt(args[0]);
             howLongInSec = Integer.parseInt(args[1]);
@@ -68,9 +69,24 @@ public class EsperClient {
         while (System.currentTimeMillis() < startTime + (1000L * howLongInSec)) {
             for (int i = 0; i < noOfRecordsPerSec; i++) {
 
-                    String resourceGroup = faker.azure().resourceGroup();
+                    Random random = new Random();
+                    int randomNumber = random.nextInt(3) + 1;
+                    String randomString = String.valueOf(randomNumber);
+//                    String resourceGroup = faker.azure().resourceGroup();
+                    String resourceGroup = "rg-" + randomString;
                     String subscriptionId = subscriptionIds[faker.number().randomDigit()%10];
-                    String region = faker.azure().region();
+                    int randomNumber2 = random.nextInt(4) + 1;
+
+                    String reg = "default";
+                    switch (randomNumber2) {
+                        case 1: reg = "germany"; break;
+                        case 2: reg = "uk"; break;
+                        case 3: reg = "asia"; break;
+                        case 4: reg = "india"; break;
+                        case 5: reg = "ukraine"; break;
+                    }
+//                    String region = faker.azure().region();
+                    String region = reg;
                     //zmiana rozkładu wartości — rzadsze wartości wysokie.
                     double cost = Math.round(Math.pow(faker.number().randomDouble(2, 0, 10),3)*100.0)/100.0;
                     Timestamp eTimestamp = faker.date().past(30, TimeUnit.SECONDS);
@@ -85,6 +101,12 @@ public class EsperClient {
                         field("ets", eTimestamp::toString),
                         field("its", iTimestamp::toString)
                 );
+//                System.out.println(resourceGroup);
+                System.out.println(region + " " + cost + " " + iTimestamp + " " + eTimestamp + " " + resourceGroup);
+//                System.out.println(region);
+//                System.out.println(cost);
+//                System.out.println(eTimestamp);
+//                System.out.println(iTimestamp);
 
                 JsonTransformer<Object> transformer = JsonTransformer.builder().build();
                 record = transformer.generate(schema, 1);
@@ -103,8 +125,119 @@ public class EsperClient {
         try {
             epCompiled = compiler.compile("""
                     @public @buseventtype create json schema AzureCostEvent(resourceGroup string, subscriptionId string, region string, cost double, ets string, its string);
-                    @name('answer')
-                    SELECT * FROM AzureCostEvent#ext_timed(java.sql.Timestamp.valueOf(its).getTime(), 3 sec)
+                    
+//                    ZAD1
+//                    @name('answer')
+//                    SELECT subscriptionId, sum(cost) as suma
+//                    FROM AzureCostEvent#ext_timed(java.sql.Timestamp.valueOf(its).getTime(), 10 sec)
+//                    GROUP BY subscriptionId
+
+
+
+                      ZAD2
+//                    @name('answer')
+//                    SELECT ets, cost, resourceGroup, subscriptionId, region
+//                    FROM AzureCostEvent#length(1)
+//                    WHERE cost >= 800
+
+
+
+//                    ZAD3
+//                    @name('answer')
+//                    SELECT ets, cost, subscriptionId, sum(cost) as suma
+//                    FROM AzureCostEvent#ext_timed(java.sql.Timestamp.valueOf(its).getTime(), 5 sec)
+//                    GROUP BY subscriptionId
+//                    HAVING (sum(cost) / (sum(cost) - cost)) > 2 AND (sum(cost) != cost)
+                    
+                    
+                    
+//                    ZAD4
+//                    create window ThirtySecondWindow#ext_timed(java.sql.Timestamp.valueOf(its).getTime(), 30)(region string, resourceGroup string, numberOfExceeds int, its string);
+                   
+//                    insert into ThirtySecondWindow
+//                    select
+//                        region,
+//                        resourceGroup,
+//                        1 as numberOfExceeds,
+//                        max(its) as its
+//                    from AzureCostEvent#ext_timed_batch(java.sql.Timestamp.valueOf(its).getTime(), 10)
+//                    group by region, resourceGroup
+//                    having sum(cost) > 500;
+//                   
+//                    @name('answer')
+//                    select region, sum(numberOfExceeds) as how_many from ThirtySecondWindow
+//                    group by region
+//                    having sum(numberOfExceeds) >=2 and count(distinct resourceGroup) >= 2
+//                    output snapshot every 10 seconds
+                    
+                    
+                    
+//                    ZAD5
+//                    create window ThirtySecondWindow#ext_timed(java.sql.Timestamp.valueOf(its).getTime(), 30) as AzureCostEvent;
+//                    insert into ThirtySecondWindow
+//                    select * from AzureCostEvent;
+//                   
+//                   
+//                    @name('answer')
+//                    select a[0].ets as start_ets,
+//                    a[0].cost as cost1,
+//                    a[1].cost as cost2,
+//                    b.ets as penalty_ets,
+//                    b.cost as penalty_cost,
+//                    b.region as region
+//                   
+//                    from pattern[
+//                    every([2:] a=ThirtySecondWindow(cost > 200 and cost <= 500) until b=ThirtySecondWindow(cost > 500))
+//                    ]
+//                    where a.distinctOf(i => region).countOf()  = 1 and a[0].region = b.region
+
+
+
+//                    ZAD6
+//                    @name('answer')
+//                    select
+//                        uk.resourceGroup as resourceGroup,
+//                        uk.ets as ets1,
+//                        germany.ets as ets2,
+//                        asia.ets as ets3,
+//                        uk.cost + germany.cost + asia.cost as cost
+//                    from pattern [every
+//                        (
+//                        uk=AzureCostEvent(region='uk' and cost > 200)
+//                        ->
+//                        (
+//                        not AzureCostEvent((cost < 10 and resourceGroup = uk.resourceGroup) or (region in ('uk','asia','india', 'germany') and resourceGroup = uk.resourceGroup))
+//                        until
+//                        germany=AzureCostEvent(region='germany' and cost > 200 and resourceGroup = uk.resourceGroup)
+//                        )
+//                        ->
+//                        (
+//                        not AzureCostEvent((cost < 10 and resourceGroup = germany.resourceGroup) or (region in ('uk','germany', 'asia', 'india') and resourceGroup=germany.resourceGroup))
+//                        until
+//                        asia=AzureCostEvent(region in ('asia', 'india') and cost > 200 and resourceGroup = germany.resourceGroup)
+//                        )
+//                        ) ]
+
+
+
+//                    ZAD7
+//                    @name('answer')
+//                    SELECT *
+//                    FROM AzureCostEvent
+//                    MATCH_RECOGNIZE(
+//                    PARTITION BY region, resourceGroup
+//                    MEASURES
+//                      STRT.region as region,
+//                      STRT.resourceGroup as resourceGroup,
+//                      STRT.ets as ets,
+//                      STRT.cost as discount,
+//                      sum(UP.cost) + STRT.cost as beforeDiscount,
+//                      sum(UP.cost) as afterDiscount,
+//                      count(UP.cost) + 1 as length
+//                    PATTERN(STRT UP{2,} DOWN)
+//                    DEFINE
+//                      UP AS UP.cost > PREV(UP.cost),
+//                      DOWN AS DOWN.cost <= PREV(DOWN.cost))
                     """, compilerArgs);
         }
         catch (EPCompileException ex) {
